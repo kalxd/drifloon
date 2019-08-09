f96e5cc14322370d01040b95aaef13b1a3dbcfae
葉岬 <XGLey@protonmail.com>
2019-08-09 22:07:10 +0800

    FC

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require("path");
+const pkg = require("./package.json");
+
+const config = {
+	entry: "./main",
+	mode: "production",
+	output: {
+		path: path.resolve("./dist"),
+		filename: `drifloon.dep.${pkg.version}.js`,
+		library: "M",
+		libraryTarget: "umd2"
+	}
+};
+
+module.exports = config;
