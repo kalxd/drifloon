b08d2d06b70269e3080a0b58da0a15ab6ac507d1
葉岬 <XGLey@protonmail.com>
2019-10-23 19:17:44 +0800

    完全移除人老珠黄的cycle.js。

--- main.js
+++ main.js
@@ -2,10 +2,7 @@ const R = require("ramda");
 const Rx = require("rxjs");
 const Ro = require("rxjs/operators");
 const Http = require("rxjs/ajax");
-const DOM = require("@cycle/dom");
-const I = require("@cycle/isolate").default;
 
-const Run = require("./drifloon/run");
 const F = require("./drifloon/function");
 const GM = require("./drifloon/gm");
 const N = require("./drifloon/node");
@@ -16,15 +13,12 @@ const M = {
 	Rx,
 	Ro,
 	Http,
-	DOM,
 	I,
 
 	N,
 	F,
 	GM,
-	E,
-
-	...Run,
+	E
 };
 
 module.exports = M;
