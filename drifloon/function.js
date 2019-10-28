5d01502b14717ca3dacae497c76681e97aff8e80
葉岬 <XGLey@protonmail.com>
2019-10-28 20:33:57 +0800

    修复fmapN无法多重compose。

--- drifloon/function.js
+++ drifloon/function.js
@@ -3,18 +3,50 @@ const R = require("ramda");
 // isAnyNil :: [Maybe a] -> Bool
 const isAnyNil = R.any(R.isNil);
 
-const fmapN = (f, ...args) => {
-	if (isAnyNil(args)) {
+// fmap :: (a -> b) -> Maybe a -> Maybe b
+const fmap = R.curry((f, a) => {
+	if (R.isNil(a)) {
 		return null;
 	}
-	return R.apply(f, args);
-};
 
-const fmap = R.curryN(1, fmapN);
-const fmap2 = R.curryN(2, fmapN);
-const fmap3 = R.curryN(3, fmapN);
-const fmap4 = R.curryN(4, fmapN);
-const fmap5 = R.curryN(5, fmapN);
+	return f(a);
+});
+
+// fmap2 :: (a -> b -> c) -> Maybe a -> Maybe b -> Maybe c
+const fmap2 = R.curry((f, a, b) => {
+	if (isAnyNil([a, b])) {
+		return null;
+	}
+
+	return f(a, b);
+});
+
+// fmap3 :: (a -> b -> c -> d) -> Maybe a -> Maybe b -> Maybe c -> Maybe d
+const fmap3 = R.curryN((f, a, b, c) => {
+	if (isAnyNil([a, b, c])) {
+		return null;
+	}
+
+	return f(a, b, c);
+});
+
+// fmap4 :: (a -> b -> c -> d -> e) -> Maybe a -> Maybe b -> Maybe c -> Maybe d -> Maybe e
+const fmap4 = R.curry((f, a, b, c, d) => {
+	if (isAnyNil([a, b, c, d])) {
+		return null;
+	}
+
+	return f(a, b, c, d);
+});
+
+// fmap5 :: (a -> b -> c -> d -> e -> f) -> Maybe a -> Maybe b -> Maybe c -> Maybe d -> Maybe e -> Maybe f
+const fmap5 = R.curry((f, a, b, c, d, e) => {
+	if (isAnyNil([a, b, c, d, e])) {
+		return null;
+	}
+
+	return f(a, b, c, d, e);
+});
 
 // traverse :: (a -> Maybe b) -> [a] -> Maybe [b]
 const traverse = R.curry((f, xs) => {
