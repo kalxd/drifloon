8d38ccdea84a0771fc7a101baec284a7eb3a4487
荀徒之 <kalxd@126.com>
2020-05-05 19:52:48 +0800

    LoadState quickcheck。

---
+++ test/Load.js
@@ -0,0 +1,42 @@
+const { testProp, fc } = require("ava-fast-check");
+const R = require("ramda");
+
+const { Load } = require("../main");
+
+// genEmpty :: () > Arbitray (LoadState Int)
+const genEmpty = () => fc.constant(Load.empty);
+
+// genLoad :: () -> Arbitray (LoadState Int)
+const genLoad = () => {
+	return fc.integer()
+		.map(Load.pure)
+	;
+};
+
+testProp(
+	"fmap id = id",
+	[genEmpty(), genLoad()],
+	(s1, s2) => {
+		const f = Load.fmap(R.identity);
+		return R.equals(f(s1), s1) && R.equals(f(s2), s2);
+	}
+);
+
+testProp(
+	"fmap f . g = fmap f . g",
+	[genEmpty(), genLoad()],
+	(s1, s2) => {
+		const h1 = Load.fmap(
+			R.compose(
+				R.dec,
+				R.inc
+			)
+		);
+		const h2 = R.compose(
+			Load.fmap(R.dec),
+			Load.fmap(R.inc)
+		);
+
+		return R.equals(h1(s1), h2(s1)) && R.equals(h1(s2), h2(s2));
+	}
+);
--- test/main.js
+++
@@ -1,7 +0,0 @@
-const { testProp, fc } = require("ava-fast-check");
-
-testProp(
-	"test one",
-	[fc.nat()],
-	a => a === a
-);
