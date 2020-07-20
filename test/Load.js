074d4b815bda64d5e55fe87f32dcf2d9967145d4
荀徒之 <kalxd@126.com>
2020-07-20 12:29:18 +0800

    合并Load多几个产生函数。

--- test/Load.js
+++ test/Load.js
@@ -3,40 +3,44 @@ const R = require("ramda");
 
 const { Load } = require("../main");
 
-// genEmpty :: () -> Arbitraty (LoadState Int)
-const genEmpty = () => fc.constant(Load.empty);
-
-// genLoad :: () -> Arbitraty (LoadState Int)
-const genLoad = () => {
-	return fc.integer()
-		.map(Load.pure)
+// gen :: Aribitraty (LoadState Int)
+const gen = () => {
+	return fc.boolean()
+		.chain(b => {
+			if (b) {
+				return fc.constant(Load.empty);
+			}
+			else {
+				return fc.integer().map(Load.pure);
+			}
+		})
 	;
 };
 
 testProp(
 	"fmap id = id",
-	[genEmpty(), genLoad()],
-	(s1, s2) => {
+	[gen()],
+	s => {
 		const f = Load.fmap(R.identity);
-		return R.equals(f(s1), s1) && R.equals(f(s2), s2);
+		return R.equals(s, f(s));
 	}
 );
 
 testProp(
 	"fmap f . g = fmap f . fmap g",
-	[genEmpty(), genLoad()],
-	(s1, s2) => {
+	[gen()],
+	s => {
+		const f = R.inc;
+		const g = R.add(10);
+
 		const h1 = Load.fmap(
-			R.compose(
-				R.dec,
-				R.inc
-			)
+			R.compose(f, g)
 		);
 		const h2 = R.compose(
-			Load.fmap(R.dec),
-			Load.fmap(R.inc)
+			Load.fmap(f),
+			Load.fmap(g)
 		);
 
-		return R.equals(h1(s1), h2(s1)) && R.equals(h1(s2), h2(s2));
+		return R.equals(h1(s), h2(s));
 	}
 );
