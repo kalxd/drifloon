e032a99835874a466a47cbf2a7a0e4be6016528f
荀徒之 <kalxd@126.com>
2020-11-06 21:46:29 +0800

    简化F模块测试。

--- test/F.js
+++ test/F.js
@@ -30,6 +30,17 @@ const mkArbIntN = R.times(arbInt);
 
 const adds = (...ns) => R.sum(ns);
 
+const checkFmap = (f, inputs) => {
+	const N = inputs.length;
+	const as = R.map(R.head, inputs);
+	const ms = R.map(R.last, inputs);
+	const g = R.curryN(N, adds);
+
+	const r1 = g(...as);
+	const r2 = f(g, ...ms);
+	return checkMaybe(r1, r2);
+};
+
 testProp(
 	"fmap",
 	[fc.option(fc.string())],
@@ -38,117 +49,50 @@ testProp(
 
 testProp(
 	"fmap2 - liftA2",
-	[arbInt(), arbInt()],
-	([a, ma], [b, mb]) => {
-		return R.pipe(
-			F.fmap2(R.add),
-			checkMaybe(R.add(a, b))
-		)(ma, mb);
-	}
+	mkArbIntN(2),
+	(...inputs) => checkFmap(F.fmap2, inputs)
 );
 
 testProp(
 	"fmap3 - liftA3",
-	[arbInt(), arbInt(), arbInt()],
-	([a, ma], [b, mb], [c, mc]) => {
-		const f = R.curry((a, b, c) => a + b + c);
-
-		return R.pipe(
-			F.fmap3(f),
-			checkMaybe(f(a, b, c))
-		)(ma, mb, mc);
-	}
+	mkArbIntN(3),
+	(...inputs) => checkFmap(F.fmap3, inputs)
 );
 
 testProp(
 	"fmap4 - liftA4",
-	[arbInt(), arbInt(), arbInt(), arbInt()],
-	([a, ma], [b, mb], [c, mc], [d, md]) => {
-		const f = R.curry((a, b, c, d) => a + b + c + d);
-
-		return R.pipe(
-			F.fmap4(f),
-			checkMaybe(f(a, b, c, d))
-		)(ma, mb, mc, md);
-	}
+	mkArbIntN(4),
+	(...inputs) => checkFmap(F.fmap4, inputs)
 );
 
 testProp(
 	"fmap5 - liftA5",
-	[arbInt(), arbInt(), arbInt(), arbInt(), arbInt()],
-	([a, ma], [b, mb], [c, mc], [d, md], [e, me]) => {
-		const f = R.curry((a, b, c, d, e) => a + b + c + d + e);
-
-		return R.pipe(
-			F.fmap5(f),
-			checkMaybe(f(a, b, c, d, e))
-		)(ma, mb, mc, md, me);
-	}
+	mkArbIntN(5),
+	(...inputs) => checkFmap(F.fmap5, inputs)
 );
 
 testProp(
 	"fmap6 - liftA6",
 	mkArbIntN(6),
-	(
-		[a1, m1],
-		[a2, m2],
-		[a3, m3],
-		[a4, m4],
-		[a5, m5],
-		[a6, m6]
-	) => {
-		const f = R.curryN(6, adds);
+	(...inputs) => checkFmap(F.fmap6, inputs)
+);
 
-		return R.pipe(
-			F.fmap6(f),
-			checkMaybe(f(a1, a2, a3, a4, a5, a6))
-		)(m1, m2, m3, m4, m5, m6);
-	}
+testProp(
+	"fmap7 - liftA7",
+	mkArbIntN(7),
+	(...inputs) => checkFmap(F.fmap7, inputs)
 );
 
 testProp(
 	"fmap8 - liftA8",
 	mkArbIntN(8),
-	(
-		[a1, m1],
-		[a2, m2],
-		[a3, m3],
-		[a4, m4],
-		[a5, m5],
-		[a6, m6],
-		[a7, m7],
-		[a8, m8],
-	) => {
-		const f = R.curryN(8, adds);
-
-		return R.pipe(
-			F.fmap8(f),
-			checkMaybe(f(a1, a2, a3, a4, a5, a6, a7, a8))
-		)(m1, m2, m3, m4, m5, m6, m7, m8);
-	}
+	(...inputs) => checkFmap(F.fmap8, inputs)
 );
 
 testProp(
 	"fmap9 - liftA9",
 	mkArbIntN(9),
-	(
-		[a1, m1],
-		[a2, m2],
-		[a3, m3],
-		[a4, m4],
-		[a5, m5],
-		[a6, m6],
-		[a7, m7],
-		[a8, m8],
-		[a9, m9]
-	) => {
-		const f = R.curryN(9, adds);
-
-		return R.pipe(
-			F.fmap9(f),
-			checkMaybe(f(a1, a2, a3, a4, a5, a6, a7, a8, a9))
-		)(m1, m2, m3, m4, m5, m6, m7, m8, m9);
-	}
+	(...inputs) => checkFmap(F.fmap9, inputs)
 );
 
 testProp(
