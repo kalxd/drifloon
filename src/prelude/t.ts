7303e8ac94c4411ec61aea3a04ee44895a87b6b6
荀涧林 <kalxd@126.com>
2022-04-03 21:31:55 +0800

    太有意思啦！

--- src/prelude/t.ts
+++ src/prelude/t.ts
@@ -7,23 +7,28 @@ const isNotNil = <T>(value: T | null | undefined): value is T => {
 	return !(value === undefined || value === null);
 };
 
-interface Maybe<T> extends Promise<T> {
+export interface Maybe<T> extends Promise<T> {
 	map: <R>(f: (x: T) => R) => Maybe<R>;
+	chain: <R>(f: (x: T) => Maybe<R>) => Maybe<R>;
 }
 
 class NilError {};
 
-const maybe = <T>(value: T | null | undefined): Maybe<T> => ({
+export const maybe = <T>(value: T | null | undefined): Maybe<T> => ({
 	then: <TResult1 = T, TResult2 = never>(
 		onfulfilled: ((x: T) => TResult1 | PromiseLike<TResult1>) ,
 		onrejected: (reason: NilError) => TResult2 | PromiseLike<TResult2>
 	): Promise<TResult1 | TResult2> => {
-		if (isNotNil(value)) {
-			return Promise.resolve(value)
-				.then(onfulfilled, onrejected);
-		}
-
-		return Promise.reject(new NilError);
+		return Promise.resolve(value)
+			.then(value => {
+				if (isNotNil(value)) {
+					return value;
+				}
+				else {
+					return Promise.reject(new NilError)
+				}
+			})
+			.then(onfulfilled, onrejected);
 	},
 
 	catch: () => {
@@ -41,7 +46,27 @@ const maybe = <T>(value: T | null | undefined): Maybe<T> => ({
 			return maybe(f(value));
 		}
 		else {
-			return maybe(value as never as R);
+			return maybe<R>(value);
+		}
+	},
+
+	chain: <R>(f: (x: T) => Maybe<R>): Maybe<R> => {
+		if (isNotNil(value)) {
+			return f(value);
+		}
+		else {
+			return maybe<R>(value);
 		}
 	}
 });
+
+export const maybeDo = async <T>(action: () => Maybe<T>): Promise<T | null | undefined> => {
+	return action().catch(e => {
+		if (e instanceof NilError) {
+			return null;
+		}
+		else {
+			return Promise.reject(e);
+		}
+	});
+};
