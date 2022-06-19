649998ee2346708608f0a61cd733ae3effe7c8a4
荀涧林 <kalxd@126.com>
2022-06-19 16:39:38 +0800

    Outter

--- src/Helper/Outter.ts
+++ src/Helper/Outter.ts
@@ -1,15 +1,35 @@
 import * as m from "mithril";
+import { Just, Maybe, Nothing } from "purify-ts";
+import IORef from "../prelude/IORef";
 
 interface OutterAttr {
 	onOutterClick: (e: MouseEvent) => void;
 }
 
-export const Outter: m.FactoryComponent<OutterAttr> = vnode => {
-	const click = vnode.attrs.onOutterClick;
+export const Outter: m.FactoryComponent<OutterAttr> = _ => {
+	const bodyClickIO: IORef<Maybe<(ev: MouseEvent) => void>> = new IORef(Nothing);
 
 	return {
 		oncreate: (vnode) => {
+			const f = (e: MouseEvent) => {
+				const target = e.target as HTMLElement;
+
+				if (!target?.contains(vnode.dom)) {
+					vnode.attrs.onOutterClick(e);
+				}
+			};
+
+			bodyClickIO.put(Just(f));
+
+			document.body.addEventListener("click", f);
 		},
-		view: () => {}
+
+		onremove: (_) => {
+			bodyClickIO.ask().ifJust(f => {
+				document.body.removeEventListener("click", f);
+			});
+		},
+
+		view: ({ children }) => children
 	};
 };
--- src/prelude/IORef.ts
+++ src/prelude/IORef.ts
@@ -1,23 +1,25 @@
-/** 从js角度看没什么卵用封装，IO是大事，要小心翼翼。 */
+export default class IORef<T> {
+	private ref: T;
 
-export interface IORef<T> {
-	map: <R>(f: (ref: T) => R) => IORef<R>;
-	andThen: <R>(f: (ref: T) => IORef<R>) => IORef<R>;
-	ap: <A, R>(f: (ref1: T, ref2: A) => R, ref2: IORef<A>) => IORef<R>;
+	constructor(value: T) {
+		this.ref = value;
+	}
+
+	modify(f: (ref: T) => T): this {
+		this.ref = f(this.ref);
+		return this;
+	}
+
+	put(other: T): this {
+		this.ref = other;
+		return this;
+	}
+
+	asks<R>(f: (ref: T) => R): R {
+		return f(this.ref);
+	}
+
+	ask(): T {
+		return this.ref;
+	}
 }
-
-export const ioref = <T>(value: T): IORef<T> => {
-	const map = <R>(f: (ref: T) => R) => ioref(f(value));
-
-	const andThen = <R>(f: (ref: T) => IORef<R>) => f(value);
-
-	const ap = <A, R>(f: (ref1: T, ref2: A) => R, ref2: IORef<A>) => {
-		return ref2.map(ref2 => f(value, ref2));
-	};
-
-	return {
-		map,
-		andThen,
-		ap
-	};
-};
