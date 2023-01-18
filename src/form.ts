8836da7915b0fe77d1e2fe48c3aa4547a192223d
荀涧林 <kalxd@126.com>
2023-01-18 19:47:19 +0800

    Field

--- src/form.ts
+++ src/form.ts
@@ -1,4 +1,4 @@
-import { cmpDefault } from "./internal/attr";
+import { cmpDefault, pickKlass, selectKlass } from "./internal/attr";
 import * as m from "mithril";
 import { identity, Maybe } from "purify-ts";
 
@@ -82,3 +82,26 @@ export const Radiobox = <T>(): m.Component<RadioboxAttr<T>> => ({
 		return children;
 	}
 });
+
+export interface FieldAttr {
+	error?: Maybe<string>;
+}
+
+export const Field: m.Component<FieldAttr> = {
+	view: ({ attrs, children }) => {
+		const error = Maybe.fromNullable(attrs.error)
+			.join();
+
+		const errorLabel = error.map(text =>
+			m("div.ui.basic.red.pointing.promot.label.transition.visible", text));
+
+		const klass = pickKlass([
+			error.map(_ => "error")
+		]);
+
+		return m("div.field", { class: klass }, [
+			children,
+			errorLabel.extract()
+		]);
+	}
+}
