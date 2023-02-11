e24805524bb4ee75f49af642fdf497f4e86a3097
荀涧林 <kalxd@126.com>
2023-02-11 19:35:29 +0800

    parse url

--- src/internal/route.ts
+++ src/internal/route.ts
@@ -1,6 +1,6 @@
 // 路由相关
 
-import { Just, Maybe, Nothing } from "purify-ts";
+import { Either, Left, Right } from "purify-ts";
 
 // TS的垃圾枚举
 interface LitPath {
@@ -13,39 +13,89 @@ interface ParamPath {
 	name: string;
 }
 
-type PathSegment = LitPath | ParamPath;
+interface VariadicPath {
+	_tag: "VariadicPath";
+	name: string;
+}
 
-const litPath = (path: string): PathSegment => ({
-	_tag: "LitPath",
-	path
-});
+type PathSegment = LitPath | ParamPath | VariadicPath;
 
-const paramPath = (name: string): PathSegment => ({
-	_tag: "ParamPath",
-	name
-});
+const parseParamPath = (input: string): Either<string, PathSegment> => {
+	const [prefix, ...xs] = input.split("");
+	if (prefix !== ":") {
+		return Left(`${input}没有:开头`);
+	}
+
+	const [a, b, c] = xs.reverse();
+	if (`${a}${b}${c}` === "...") {
+		return Left(`${input}不能解释为VairadicPath`);
+	}
+	return Right({
+		_tag: "ParamPath",
+		name: xs.join("")
+	});
+};
+
+const parseVariadicPath = (input: string): Either<string, PathSegment> => {
+	const [prefix, ...xs] = input.split("");
+	if (prefix !== ":") {
+		return Left(`${input}没有:开头`);
+	}
+	const [a, b, c, ...ys] = xs.reverse();
+	if (`${a}${b}${c}` !== "...") {
+		return Left(`${input}没有...结尾`);
+	}
+
+	return Right({
+		_tag: "VariadicPath",
+		name: ys.reverse().join("")
+	});
+};
+
+const parsePath = (input: string): PathSegment => {
+	return parseVariadicPath(input)
+		.altLazy(() => parseParamPath(input))
+		.orDefaultLazy(() => ({ _tag: "LitPath", path: input }));
+};
 
 export interface PathSegments {
 	segments: Array<PathSegment>
 }
 
-const tryParseParam = (s: string): Maybe<string> => {
-	if (s.startsWith(":")) {
-		return Just(s.slice(1));
-	}
-	else {
-		return Nothing;
-	}
+const splitPath = (path: string): Array<string> => {
+	return path.split("/")
+		.map(s => s.trim())
+		.filter(s => s.length !== 0);
 };
 
 export const pathIntoSegments = (path: string): PathSegments => {
-	const ss: Array<PathSegment> = path.split("/")
-		.map(s => tryParseParam(s)
-			 .caseOf({
-				 Just: paramPath,
-				 Nothing: () => litPath(s)
-			 })
-		);
+	const ss: Array<PathSegment> = splitPath(path)
+		.map(parsePath);
 
 	return { segments: ss };
+};
+
+export const isMatchUrl = (pathsegments: PathSegments, path: string): boolean => {
+	const paths = splitPath(path);
+
+	for (let i = 0; i < pathsegments.segments.length; ++i) {
+		const segment = pathsegments.segments[i];
+		if (segment._tag === "LitPath") {
+			if (paths[i] === null || paths[i] !== segment.path) {
+				return false;
+			}
+		}
+		else if (segment._tag === "ParamPath") {
+			if (paths[i] === null) {
+				return false;
+			}
+		}
+		else {
+			if (paths[i] !== null) {
+				return true;
+			}
+		}
+	}
+
+	return true;
 };
