0901c3b9308768174b86f06ad0feffda0d6ed781
荀涧林 <kalxd@126.com>
2023-02-12 10:50:47 +0800

    表单基本式子。

--- src/form.ts
+++ src/form.ts
@@ -1,6 +1,9 @@
-import { pickKlass } from "./internal/attr";
+import { pickKlass, selectKlass } from "./internal/attr";
 import * as m from "mithril";
-import { Maybe } from "purify-ts";
+import { identity, Maybe } from "purify-ts";
+import { LoadingShape, Size, StateLevel } from "./data/var";
+import { FormData } from "./data/form";
+import { Message, MessageAttr } from "./element/message";
 
 export interface FieldAttr {
 	error?: Maybe<string>;
@@ -24,3 +27,62 @@ export const Field: m.Component<FieldAttr> = {
 		]);
 	}
 }
+
+export interface TextFieldAttr {
+	label?: string;
+	isRequire?: boolean;
+	onchange?: (output: string) => void;
+}
+
+export const TextField: m.Component<TextFieldAttr> = {
+	view: ({ attrs }) => {
+		const klass = pickKlass([
+			selectKlass("required", attrs.isRequire)
+		]);
+
+		const onchange = (e: Event) => {
+			const input = e.target as HTMLInputElement;
+			const f = attrs.onchange ?? identity;
+			f(input.value.trim());
+		};
+
+		return m("div.field", { class: klass }, [
+			m("label", attrs.label),
+			m("input", { onchange })
+		]);
+	}
+};
+
+export interface FormAttr<T> {
+	loading?: LoadingShape;
+	isInvert?: boolean;
+	size?: Size;
+	formdata?: FormData<T>;
+}
+
+export const Form = <T>(): m.Component<FormAttr<T>> => {
+	return {
+		view: ({ attrs, children }) => {
+			const err = Maybe.fromNullable(attrs.formdata)
+				.chain(data => data.err);
+
+			const klass = pickKlass([
+				Maybe.fromNullable(attrs.loading),
+				selectKlass("inverted", attrs.isInvert),
+				Maybe.fromNullable(attrs.size),
+				err.map(_ => "error")
+			]);
+
+			const errMsg = err.map(msg => m(
+				Message,
+				{ state: StateLevel.Error },
+				msg
+			));
+
+			return m("div.ui.form", { class: klass }, [
+				(children as m.Children),
+				errMsg.extract()
+			]);
+		}
+	};
+};
