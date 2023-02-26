667213e482c87f2d20f2cf2e1603cdd54cc8c07a
荀涧林 <kalxd@126.com>
2023-02-26 19:07:00 +0800

    loading

--- site/src/page/placeholder.ts
+++ site/src/page/placeholder.ts
@@ -2,28 +2,45 @@ import { Size } from "drifloon/data/var";
 import { Header } from "drifloon/header";
 import { Segment } from "drifloon/segment";
 import { FluidPlaceholder } from "drifloon/placeholder";
+import { loading } from "drifloon/widget/load";
 import * as m from "mithril";
+import { IORef } from "drifloon/data/ref";
+import { Just, Maybe, Nothing } from "purify-ts";
+import { Button } from "drifloon/button";
 
-const Sample: m.Component = {
+const Timer = () => {
+	const list = new IORef<Maybe<Array<number>>>(Nothing);
+
+	const delay = () => new Promise(resolve => setTimeout(resolve, 1000));
+
+	const fetchData = async () => {
+		await delay();
+		list.put(Just([1, 2, 3, 4, 5]));
+		m.redraw();
+	};
+
+	const startTimer = () => {
+		list.ask()
+			.ifJust(async () => {
+				list.put(Nothing);
+				await fetchData();
+			});
+	};
+
+	const render = (xs: Array<number>): m.Vnode =>
+		m("ol", xs.map(x => m("li", x)));
+
+
+	fetchData();
+
+	return {
 		view: () => {
-			console.log("do rendering");
-			return m("div", "you see me!");
+			return m("div", [
+				m(Button, { onclick: startTimer }, "执行"),
+				loading(render, list.ask())
+			]);
 		}
-};
-
-interface Attr {
-	isShow: boolean;
-}
-
-const Show: m.Component<Attr> = {
-	view: ({ attrs, children }) => {
-		if (attrs.isShow) {
-			return children;
-		}
-		else {
-			return null;
-		}
-	}
+	};
 };
 
 const Main: m.Component = {
@@ -33,7 +50,7 @@ const Main: m.Component = {
 			m(FluidPlaceholder),
 
 			m(Header, "演示"),
-			m(Show, { isShow: false }, m(Sample))
+			m(Timer)
 		]);
 	}
 };
