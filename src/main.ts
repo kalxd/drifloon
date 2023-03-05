28308c67bfd221c49324bd23b5c79e7b44d91ae7
荀涧林 <kalxd@126.com>
2023-03-05 14:13:20 +0800

    .

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import * as m from "mithril";
+import { ModalMask } from "./modal";
+
+export const run = (r: m.RouteDefs): void => {
+	m.route(document.body, "/", r);
+};
+
+export const MainLayout = <T>(): m.Component<T> => ({
+	view: ({ children }) => m.fragment({}, [
+		children,
+		m(ModalMask)
+	])
+});
+
+export const mount = <T>(c: m.Component<T>): void => {
+	const App: m.Component<T> = {
+		view: () => m(MainLayout, m(c))
+	};
+	m.mount(document.body, App);
+};
