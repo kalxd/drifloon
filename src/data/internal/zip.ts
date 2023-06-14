6504247f519b70556105cdc56ecda30d3cd7b92c
荀涧林 <kalxd@126.com>
2023-06-14 20:14:18 +0800

    一个傻屌zip写了快七百行，TS不是垃圾是个啥？！

--- src/data/internal/zip.ts
+++ src/data/internal/zip.ts
@@ -1,4 +1,4 @@
-import { Either, Maybe } from "purify-ts"
+import { Either, EitherAsync, Maybe, MaybeAsync } from "purify-ts"
 
 export const maybeZip = <T1, T2>(
 	ma1: Maybe<T1>,
@@ -178,8 +178,8 @@ export const eitherZip3 = <E, T1, T2, T3>(
 	ma2: Either<E, T2>,
 	ma3: Either<E, T3>
 ): Either<E, [T1, T2, T3]> =>
-	ma1.chain(a1 => ma2.map(a2 => [a1, a2] as [T1, T2]))
-		.chain(xs => ma3.map(a3 => [...xs, a3]));
+	eitherZip(ma1, ma2)
+		.chain(xs => ma3.map(x => [...xs, x]));
 
 export const eitherZip4 = <E, T1, T2, T3, T4>(
 	ma1: Either<E, T1>,
@@ -187,9 +187,8 @@ export const eitherZip4 = <E, T1, T2, T3, T4>(
 	ma3: Either<E, T3>,
 	ma4: Either<E, T4>
 ): Either<E, [T1, T2, T3, T4]> =>
-	ma1.chain(a1 => ma2.map(a2 => [a1, a2] as [T1, T2]))
-		.chain(xs => ma3.map(a3 => [...xs, a3] as [T1, T2, T3]))
-		.chain(xs => ma4.map(a4 => [...xs, a4]));
+	eitherZip3(ma1, ma2, ma3)
+		.chain(xs => ma4.map(x => [...xs, x]));
 
 export const eitherZip5 = <E, T1, T2, T3, T4, T5>(
 	ma1: Either<E, T1>,
@@ -198,10 +197,8 @@ export const eitherZip5 = <E, T1, T2, T3, T4, T5>(
 	ma4: Either<E, T4>,
 	ma5: Either<E, T5>
 ): Either<E, [T1, T2, T3, T4, T5]> =>
-	ma1.chain(a1 => ma2.map(a2 => [a1, a2] as [T1, T2]))
-		.chain(xs => ma3.map(a3 => [...xs, a3] as [T1, T2, T3]))
-		.chain(xs => ma4.map(a4 => [...xs, a4] as [T1, T2, T3, T4]))
-		.chain(xs => ma5.map(a5 => [...xs, a5]));
+	eitherZip4(ma1, ma2, ma3, ma4)
+		.chain(xs => ma5.map(x => [...xs, x]));
 
 export const eitherZip6 = <E, T1, T2, T3, T4, T5, T6>(
 	ma1: Either<E, T1>,
@@ -211,10 +208,7 @@ export const eitherZip6 = <E, T1, T2, T3, T4, T5, T6>(
 	ma5: Either<E, T5>,
 	ma6: Either<E, T6>
 ): Either<E, [T1, T2, T3, T4, T5, T6]> =>
-	ma1.chain(a1 => ma2.map(a2 => [a1, a2] as [T1, T2]))
-		.chain(xs => ma3.map(a3 => [...xs, a3] as [T1, T2, T3]))
-		.chain(xs => ma4.map(a4 => [...xs, a4] as [T1, T2, T3, T4]))
-		.chain(xs => ma5.map(a5 => [...xs, a5] as [T1, T2, T3, T4, T5]))
+	eitherZip5(ma1, ma2, ma3, ma4, ma5)
 		.chain(xs => ma6.map(a6 => [...xs, a6]));
 
 export const eitherZip7 = <E, T1, T2, T3, T4, T5, T6, T7>(
@@ -226,11 +220,7 @@ export const eitherZip7 = <E, T1, T2, T3, T4, T5, T6, T7>(
 	ma6: Either<E, T6>,
 	ma7: Either<E, T7>
 ): Either<E, [T1, T2, T3, T4, T5, T6, T7]> =>
-	ma1.chain(a1 => ma2.map(a2 => [a1, a2] as [T1, T2]))
-		.chain(xs => ma3.map(a3 => [...xs, a3] as [T1, T2, T3]))
-		.chain(xs => ma4.map(a4 => [...xs, a4] as [T1, T2, T3, T4]))
-		.chain(xs => ma5.map(a5 => [...xs, a5] as [T1, T2, T3, T4, T5]))
-		.chain(xs => ma6.map(a6 => [...xs, a6] as [T1, T2, T3, T4, T5, T6]))
+	eitherZip6(ma1, ma2, ma3, ma4, ma5, ma6)
 		.chain(xs => ma7.map(a7 => [...xs, a7]));
 
 export const eitherZip8 = <E, T1, T2, T3, T4, T5, T6, T7, T8>(
@@ -243,12 +233,7 @@ export const eitherZip8 = <E, T1, T2, T3, T4, T5, T6, T7, T8>(
 	ma7: Either<E, T7>,
 	ma8: Either<E, T8>
 ): Either<E, [T1, T2, T3, T4, T5, T6, T7, T8]> =>
-	ma1.chain(a1 => ma2.map(a2 => [a1, a2] as [T1, T2]))
-		.chain(xs => ma3.map(a3 => [...xs, a3] as [T1, T2, T3]))
-		.chain(xs => ma4.map(a4 => [...xs, a4] as [T1, T2, T3, T4]))
-		.chain(xs => ma5.map(a5 => [...xs, a5] as [T1, T2, T3, T4, T5]))
-		.chain(xs => ma6.map(a6 => [...xs, a6] as [T1, T2, T3, T4, T5, T6]))
-		.chain(xs => ma7.map(a7 => [...xs, a7] as [T1, T2, T3, T4, T5, T6, T7]))
+	eitherZip7(ma1, ma2, ma3, ma4, ma5, ma6, ma7)
 		.chain(xs => ma8.map(a8 => [...xs, a8]));
 
 export const eitherZip9 = <E, T1, T2, T3, T4, T5, T6, T7, T8, T9>(
@@ -262,13 +247,7 @@ export const eitherZip9 = <E, T1, T2, T3, T4, T5, T6, T7, T8, T9>(
 	ma8: Either<E, T8>,
 	ma9: Either<E, T9>
 ): Either<E, [T1, T2, T3, T4, T5, T6, T7, T8, T9]> =>
-	ma1.chain(a1 => ma2.map(a2 => [a1, a2] as [T1, T2]))
-		.chain(xs => ma3.map(a3 => [...xs, a3] as [T1, T2, T3]))
-		.chain(xs => ma4.map(a4 => [...xs, a4] as [T1, T2, T3, T4]))
-		.chain(xs => ma5.map(a5 => [...xs, a5] as [T1, T2, T3, T4, T5]))
-		.chain(xs => ma6.map(a6 => [...xs, a6] as [T1, T2, T3, T4, T5, T6]))
-		.chain(xs => ma7.map(a7 => [...xs, a7] as [T1, T2, T3, T4, T5, T6, T7]))
-		.chain(xs => ma8.map(a8 => [...xs, a8] as [T1, T2, T3, T4, T5, T6, T7, T8]))
+	eitherZip8(ma1, ma2, ma3, ma4, ma5, ma6, ma7, ma8)
 		.chain(xs => ma9.map(a9 => [...xs, a9]));
 
 export const eitherZipWith = <E, T1, T2, R>(
@@ -354,3 +333,337 @@ export const eitherZipWith9 = <E, T1, T2, T3, T4, T5, T6, T7, T8, T9, R>(
 	ma9: Either<E, T9>
 ): Either<E, R> =>
 	eitherZip9(ma1, ma2, ma3, ma4, ma5, ma6, ma7, ma8, ma9).map(xs => f(...xs));
+
+export const maybeAsyncZip = <T1, T2>(
+	ma1: MaybeAsync<T1>,
+	ma2: MaybeAsync<T2>
+): MaybeAsync<[T1, T2]> =>
+	ma1.chain(a1 => ma2.map(a2 => [a1, a2]));
+
+export const maybeAsyncZip3 = <T1, T2, T3>(
+	ma1: MaybeAsync<T1>,
+	ma2: MaybeAsync<T2>,
+	ma3: MaybeAsync<T3>
+): MaybeAsync<[T1, T2, T3]> =>
+	maybeAsyncZip(ma1, ma2)
+		.chain(xs => ma3.map(a3 => [...xs, a3]));
+
+export const maybeAsyncZip4 = <T1, T2, T3, T4>(
+	ma1: MaybeAsync<T1>,
+	ma2: MaybeAsync<T2>,
+	ma3: MaybeAsync<T3>,
+	ma4: MaybeAsync<T4>,
+): MaybeAsync<[T1, T2, T3, T4]> =>
+	maybeAsyncZip3(ma1, ma2, ma3)
+		.chain(xs => ma4.map(a4 => [...xs, a4]));
+
+export const maybeAsyncZip5 = <T1, T2, T3, T4, T5>(
+	ma1: MaybeAsync<T1>,
+	ma2: MaybeAsync<T2>,
+	ma3: MaybeAsync<T3>,
+	ma4: MaybeAsync<T4>,
+	ma5: MaybeAsync<T5>
+): MaybeAsync<[T1, T2, T3, T4, T5]> =>
+	maybeAsyncZip4(ma1, ma2, ma3, ma4)
+		.chain(xs => ma5.map(a5 => [...xs, a5]));
+
+export const maybeAsyncZip6 = <T1, T2, T3, T4, T5, T6>(
+	ma1: MaybeAsync<T1>,
+	ma2: MaybeAsync<T2>,
+	ma3: MaybeAsync<T3>,
+	ma4: MaybeAsync<T4>,
+	ma5: MaybeAsync<T5>,
+	ma6: MaybeAsync<T6>
+): MaybeAsync<[T1, T2, T3, T4, T5, T6]> =>
+	maybeAsyncZip5(ma1, ma2, ma3, ma4, ma5)
+		.chain(xs => ma6.map(a6 => [...xs, a6]));
+
+export const maybeAsyncZip7 = <T1, T2, T3, T4, T5, T6, T7>(
+	ma1: MaybeAsync<T1>,
+	ma2: MaybeAsync<T2>,
+	ma3: MaybeAsync<T3>,
+	ma4: MaybeAsync<T4>,
+	ma5: MaybeAsync<T5>,
+	ma6: MaybeAsync<T6>,
+	ma7: MaybeAsync<T7>
+): MaybeAsync<[T1, T2, T3, T4, T5, T6, T7]> =>
+	maybeAsyncZip6(ma1, ma2, ma3, ma4, ma5, ma6)
+		.chain(xs => ma7.map(a7 => [...xs, a7]));
+
+export const maybeAsyncZip8 = <T1, T2, T3, T4, T5, T6, T7, T8>(
+	ma1: MaybeAsync<T1>,
+	ma2: MaybeAsync<T2>,
+	ma3: MaybeAsync<T3>,
+	ma4: MaybeAsync<T4>,
+	ma5: MaybeAsync<T5>,
+	ma6: MaybeAsync<T6>,
+	ma7: MaybeAsync<T7>,
+	ma8: MaybeAsync<T8>
+): MaybeAsync<[T1, T2, T3, T4, T5, T6, T7, T8]> =>
+	maybeAsyncZip7(ma1, ma2, ma3, ma4, ma5, ma6, ma7)
+		.chain(xs => ma8.map(x => [...xs, x]));
+
+export const maybeAsyncZip9 = <T1, T2, T3, T4, T5, T6, T7, T8, T9>(
+	ma1: MaybeAsync<T1>,
+	ma2: MaybeAsync<T2>,
+	ma3: MaybeAsync<T3>,
+	ma4: MaybeAsync<T4>,
+	ma5: MaybeAsync<T5>,
+	ma6: MaybeAsync<T6>,
+	ma7: MaybeAsync<T7>,
+	ma8: MaybeAsync<T8>,
+	ma9: MaybeAsync<T9>
+): MaybeAsync<[T1, T2, T3, T4, T5, T6, T7, T8, T9]> =>
+	maybeAsyncZip8(ma1, ma2, ma3, ma4, ma5, ma6, ma7, ma8)
+		.chain(xs => ma9.map(x => [...xs, x]));
+
+export const maybeAsyncZipWith = <T1, T2, R>(
+	f: (a1: T1, a2: T2) => R,
+	ma1: MaybeAsync<T1>,
+	ma2: MaybeAsync<T2>
+): MaybeAsync<R> =>
+	maybeAsyncZip(ma1, ma2).map(xs => f(...xs));
+
+export const maybeAsyncZipWith3 = <T1, T2, T3, R>(
+	f: (a1: T1, a2: T2, a3: T3) => R,
+	ma1: MaybeAsync<T1>,
+	ma2: MaybeAsync<T2>,
+	ma3: MaybeAsync<T3>
+): MaybeAsync<R> =>
+	maybeAsyncZip3(ma1, ma2, ma3).map(xs => f(...xs));
+
+export const maybeAsyncZipWith4 = <T1, T2, T3, T4, R>(
+	f: (a1: T1, a2: T2, a3: T3, a4: T4) => R,
+	ma1: MaybeAsync<T1>,
+	ma2: MaybeAsync<T2>,
+	ma3: MaybeAsync<T3>,
+	ma4: MaybeAsync<T4>
+): MaybeAsync<R> =>
+	maybeAsyncZip4(ma1, ma2, ma3, ma4).map(xs => f(...xs));
+
+export const maybeAsyncZipWith5 = <T1, T2, T3, T4, T5, R>(
+	f: (a1: T1, a2: T2, a3: T3, a4: T4, a5: T5) => R,
+	ma1: MaybeAsync<T1>,
+	ma2: MaybeAsync<T2>,
+	ma3: MaybeAsync<T3>,
+	ma4: MaybeAsync<T4>,
+	ma5: MaybeAsync<T5>
+): MaybeAsync<R> =>
+	maybeAsyncZip5(ma1, ma2, ma3, ma4, ma5).map(xs => f(...xs));
+
+export const maybeAsyncZipWith6 = <T1, T2, T3, T4, T5, T6, R>(
+	f: (a1: T1, a2: T2, a3: T3, a4: T4, a5: T5, a6: T6) => R,
+	ma1: MaybeAsync<T1>,
+	ma2: MaybeAsync<T2>,
+	ma3: MaybeAsync<T3>,
+	ma4: MaybeAsync<T4>,
+	ma5: MaybeAsync<T5>,
+	ma6: MaybeAsync<T6>
+): MaybeAsync<R> =>
+	maybeAsyncZip6(ma1, ma2, ma3, ma4, ma5, ma6).map(xs => f(...xs));
+
+export const maybeAsyncZipWith7 = <T1, T2, T3, T4, T5, T6, T7, R>(
+	f: (a1: T1, a2: T2, a3: T3, a4: T4, a5: T5, a6: T6, a7: T7) => R,
+	ma1: MaybeAsync<T1>,
+	ma2: MaybeAsync<T2>,
+	ma3: MaybeAsync<T3>,
+	ma4: MaybeAsync<T4>,
+	ma5: MaybeAsync<T5>,
+	ma6: MaybeAsync<T6>,
+	ma7: MaybeAsync<T7>
+): MaybeAsync<R> =>
+	maybeAsyncZip7(ma1, ma2, ma3, ma4, ma5, ma6, ma7).map(xs => f(...xs));
+
+export const maybeAsyncZipWith8 = <T1, T2, T3, T4, T5, T6, T7, T8, R>(
+	f: (a1: T1, a2: T2, a3: T3, a4: T4, a5: T5, a6: T6, a7: T7, a8: T8) => R,
+	ma1: MaybeAsync<T1>,
+	ma2: MaybeAsync<T2>,
+	ma3: MaybeAsync<T3>,
+	ma4: MaybeAsync<T4>,
+	ma5: MaybeAsync<T5>,
+	ma6: MaybeAsync<T6>,
+	ma7: MaybeAsync<T7>,
+	ma8: MaybeAsync<T8>
+): MaybeAsync<R> =>
+	maybeAsyncZip8(ma1, ma2, ma3, ma4, ma5, ma6, ma7, ma8).map(xs => f(...xs));
+
+export const maybeAsyncZipWith9 = <T1, T2, T3, T4, T5, T6, T7, T8, T9, R>(
+	f: (a1: T1, a2: T2, a3: T3, a4: T4, a5: T5, a6: T6, a7: T7, a8: T8, a9: T9) => R,
+	ma1: MaybeAsync<T1>,
+	ma2: MaybeAsync<T2>,
+	ma3: MaybeAsync<T3>,
+	ma4: MaybeAsync<T4>,
+	ma5: MaybeAsync<T5>,
+	ma6: MaybeAsync<T6>,
+	ma7: MaybeAsync<T7>,
+	ma8: MaybeAsync<T8>,
+	ma9: MaybeAsync<T9>
+): MaybeAsync<R> =>
+	maybeAsyncZip9(ma1, ma2, ma3, ma4, ma5, ma6, ma7, ma8, ma9).map(xs => f(...xs));
+
+export const eitherAsyncZip = <E, T1, T2>(
+	ma1: EitherAsync<E, T1>,
+	ma2: EitherAsync<E, T2>
+): EitherAsync<E, [T1, T2]> =>
+	ma1.chain(a1 => ma2.map(a2 => [a1, a2]));
+
+export const eitherAsyncZip3 = <E, T1, T2, T3>(
+	ma1: EitherAsync<E, T1>,
+	ma2: EitherAsync<E, T2>,
+	ma3: EitherAsync<E, T3>,
+): EitherAsync<E, [T1, T2, T3]> =>
+	eitherAsyncZip(ma1, ma2)
+		.chain(xs => ma3.map(x => [...xs, x]));
+
+export const eitherAsyncZip4 = <E, T1, T2, T3, T4>(
+	ma1: EitherAsync<E, T1>,
+	ma2: EitherAsync<E, T2>,
+	ma3: EitherAsync<E, T3>,
+	ma4: EitherAsync<E, T4>
+): EitherAsync<E, [T1, T2, T3, T4]> =>
+	eitherAsyncZip3(ma1, ma2, ma3)
+		.chain(xs => ma4.map(x => [...xs, x]));
+
+export const eitherAsyncZip5 = <E, T1, T2, T3, T4, T5>(
+	ma1: EitherAsync<E, T1>,
+	ma2: EitherAsync<E, T2>,
+	ma3: EitherAsync<E, T3>,
+	ma4: EitherAsync<E, T4>,
+	ma5: EitherAsync<E, T5>
+): EitherAsync<E, [T1, T2, T3, T4, T5]> =>
+	eitherAsyncZip4(ma1, ma2, ma3, ma4)
+		.chain(xs => ma5.map(x => [...xs, x]));
+
+export const eitherAsyncZip6 = <E, T1, T2, T3, T4, T5, T6>(
+	ma1: EitherAsync<E, T1>,
+	ma2: EitherAsync<E, T2>,
+	ma3: EitherAsync<E, T3>,
+	ma4: EitherAsync<E, T4>,
+	ma5: EitherAsync<E, T5>,
+	ma6: EitherAsync<E, T6>
+): EitherAsync<E, [T1, T2, T3, T4, T5, T6]> =>
+	eitherAsyncZip5(ma1, ma2, ma3, ma4, ma5)
+		.chain(xs => ma6.map(x => [...xs, x]));
+
+export const eitherAsyncZip7 = <E, T1, T2, T3, T4, T5, T6, T7>(
+	ma1: EitherAsync<E, T1>,
+	ma2: EitherAsync<E, T2>,
+	ma3: EitherAsync<E, T3>,
+	ma4: EitherAsync<E, T4>,
+	ma5: EitherAsync<E, T5>,
+	ma6: EitherAsync<E, T6>,
+	ma7: EitherAsync<E, T7>
+): EitherAsync<E, [T1, T2, T3, T4, T5, T6, T7]> =>
+	eitherAsyncZip6(ma1, ma2, ma3, ma4, ma5, ma6)
+		.chain(xs => ma7.map(x => [...xs, x]));
+
+export const eitherAsyncZip8 = <E, T1, T2, T3, T4, T5, T6, T7, T8>(
+	ma1: EitherAsync<E, T1>,
+	ma2: EitherAsync<E, T2>,
+	ma3: EitherAsync<E, T3>,
+	ma4: EitherAsync<E, T4>,
+	ma5: EitherAsync<E, T5>,
+	ma6: EitherAsync<E, T6>,
+	ma7: EitherAsync<E, T7>,
+	ma8: EitherAsync<E, T8>
+): EitherAsync<E, [T1, T2, T3, T4, T5, T6, T7, T8]> =>
+	eitherAsyncZip7(ma1, ma2, ma3, ma4, ma5, ma6, ma7)
+		.chain(xs => ma8.map(x => [...xs, x]));
+
+export const eitherAsyncZip9 = <E, T1, T2, T3, T4, T5, T6, T7, T8, T9>(
+	ma1: EitherAsync<E, T1>,
+	ma2: EitherAsync<E, T2>,
+	ma3: EitherAsync<E, T3>,
+	ma4: EitherAsync<E, T4>,
+	ma5: EitherAsync<E, T5>,
+	ma6: EitherAsync<E, T6>,
+	ma7: EitherAsync<E, T7>,
+	ma8: EitherAsync<E, T8>,
+	ma9: EitherAsync<E, T9>
+): EitherAsync<E, [T1, T2, T3, T4, T5, T6, T7, T8, T9]> =>
+	eitherAsyncZip8(ma1, ma2, ma3, ma4, ma5, ma6, ma7, ma8)
+		.chain(xs => ma9.map(x => [...xs, x]));
+
+export const eitherAsyncZipWith = <E, T1, T2, R>(
+	f: (a1: T1, a2: T2) => R,
+	ma1: EitherAsync<E, T1>,
+	ma2: EitherAsync<E, T2>
+): EitherAsync<E, R> =>
+	eitherAsyncZip(ma1, ma2).map(xs => f(...xs));
+
+export const eitherAsyncZipWith3 = <E, T1, T2, T3, R>(
+	f: (a1: T1, a2: T2, a3: T3) => R,
+	ma1: EitherAsync<E, T1>,
+	ma2: EitherAsync<E, T2>,
+	ma3: EitherAsync<E, T3>
+): EitherAsync<E, R> =>
+	eitherAsyncZip3(ma1, ma2, ma3).map(xs => f(...xs));
+
+export const eitherAsyncZipWith4 = <E, T1, T2, T3, T4, R>(
+	f: (a1: T1, a2: T2, a3: T3, a4: T4) => R,
+	ma1: EitherAsync<E, T1>,
+	ma2: EitherAsync<E, T2>,
+	ma3: EitherAsync<E, T3>,
+	ma4: EitherAsync<E, T4>
+): EitherAsync<E, R> =>
+	eitherAsyncZip4(ma1, ma2, ma3, ma4).map(xs => f(...xs));
+
+export const eitherAsyncZipWith5 = <E, T1, T2, T3, T4, T5, R>(
+	f: (a1: T1, a2: T2, a3: T3, a4: T4, a5: T5) => R,
+	ma1: EitherAsync<E, T1>,
+	ma2: EitherAsync<E, T2>,
+	ma3: EitherAsync<E, T3>,
+	ma4: EitherAsync<E, T4>,
+	ma5: EitherAsync<E, T5>
+): EitherAsync<E, R> =>
+	eitherAsyncZip5(ma1, ma2, ma3, ma4, ma5).map(xs => f(...xs));
+
+export const eitherAsyncZipWith6 = <E, T1, T2, T3, T4, T5, T6, R>(
+	f: (a1: T1, a2: T2, a3: T3, a4: T4, a5: T5, a6: T6) => R,
+	ma1: EitherAsync<E, T1>,
+	ma2: EitherAsync<E, T2>,
+	ma3: EitherAsync<E, T3>,
+	ma4: EitherAsync<E, T4>,
+	ma5: EitherAsync<E, T5>,
+	ma6: EitherAsync<E, T6>
+): EitherAsync<E, R> =>
+	eitherAsyncZip6(ma1, ma2, ma3, ma4, ma5, ma6).map(xs => f(...xs));
+
+export const eitherAsyncZipWith7 = <E, T1, T2, T3, T4, T5, T6, T7, R>(
+	f: (a1: T1, a2: T2, a3: T3, a4: T4, a5: T5, a6: T6, a7: T7) => R,
+	ma1: EitherAsync<E, T1>,
+	ma2: EitherAsync<E, T2>,
+	ma3: EitherAsync<E, T3>,
+	ma4: EitherAsync<E, T4>,
+	ma5: EitherAsync<E, T5>,
+	ma6: EitherAsync<E, T6>,
+	ma7: EitherAsync<E, T7>
+): EitherAsync<E, R> =>
+	eitherAsyncZip7(ma1, ma2, ma3, ma4, ma5, ma6, ma7).map(xs => f(...xs));
+
+export const eitherAsyncZipWith8 = <E, T1, T2, T3, T4, T5, T6, T7, T8, R>(
+	f: (a1: T1, a2: T2, a3: T3, a4: T4, a5: T5, a6: T6, a7: T7, a8: T8) => R,
+	ma1: EitherAsync<E, T1>,
+	ma2: EitherAsync<E, T2>,
+	ma3: EitherAsync<E, T3>,
+	ma4: EitherAsync<E, T4>,
+	ma5: EitherAsync<E, T5>,
+	ma6: EitherAsync<E, T6>,
+	ma7: EitherAsync<E, T7>,
+	ma8: EitherAsync<E, T8>
+): EitherAsync<E, R> =>
+	eitherAsyncZip8(ma1, ma2, ma3, ma4, ma5, ma6, ma7, ma8).map(xs => f(...xs));
+
+export const eitherAsyncZipWith9 = <E, T1, T2, T3, T4, T5, T6, T7, T8, T9, R>(
+	f: (a1: T1, a2: T2, a3: T3, a4: T4, a5: T5, a6: T6, a7: T7, a8: T8, a9: T9) => R,
+	ma1: EitherAsync<E, T1>,
+	ma2: EitherAsync<E, T2>,
+	ma3: EitherAsync<E, T3>,
+	ma4: EitherAsync<E, T4>,
+	ma5: EitherAsync<E, T5>,
+	ma6: EitherAsync<E, T6>,
+	ma7: EitherAsync<E, T7>,
+	ma8: EitherAsync<E, T8>,
+	ma9: EitherAsync<E, T9>
+): EitherAsync<E, R> =>
+	eitherAsyncZip9(ma1, ma2, ma3, ma4, ma5, ma6, ma7, ma8, ma9).map(xs => f(...xs));
