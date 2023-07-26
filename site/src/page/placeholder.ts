5999ba36b6e6505e731eb649f5290de81e12de6f
荀涧林 <kalxd@126.com>
2023-07-26 19:57:02 +0800

    调整。

--- site/src/page/placeholder.ts
+++ site/src/page/placeholder.ts
@@ -2,6 +2,7 @@ import { Size } from "drifloon/data/var";
 import { Header, Header2 } from "drifloon/element/header";
 import { Segment } from "drifloon/element/segment";
 import { FluidPlaceholder } from "drifloon/element/placeholder";
+import { Message } from "drifloon/element/message";
 import { loading } from "drifloon/widget/load";
 import * as m from "mithril";
 import { IORef } from "drifloon/data/ref";
@@ -47,22 +48,21 @@ const Timer = () => {
 const WaitS = (): m.Component => {
 	const [update, Wait] = waitting();
 	const state = new IORef<number>(0);
-	const f: WaittingFn = () =>
-		EitherAsync.fromPromise(async () => {
-			await delay();
-			const n = state.update(s => s + 1).ask();
-			if ( n % 2 === 0) {
-				return Left("偶数次必然出错!");
-			}
-			else {
-				return Right(m("ui.message", [
-					m("div.header", `你一共请求了${n}次`),
-					m("div", [
-						m(Button, { connectClick: () => update(f) }, "再请求一次！")
-					])
-				]));
-			}
-		});
+	const f: WaittingFn = () => EitherAsync.fromPromise(async () => {
+		await delay();
+		const n = state.update(s => s + 1).ask();
+		if (n % 2 === 0) {
+			return Left("偶数次必然出错!");
+		}
+		else {
+			return Right(m(Message, [
+				m("div.header", `你一共请求了${n}次`),
+				m("div", [
+					m(Button, { connectClick: () => update(f) }, "再请求一次！")
+				])
+			]));
+		}
+	});
 
 	update(f);
 
