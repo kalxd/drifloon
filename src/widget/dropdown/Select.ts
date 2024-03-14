ce4faf220549b1a2274d8548a2888884e6e5a41f
荀涧林 <kalxd@126.com>
2024-03-14 13:53:19 +0800

    删除未使用代码。

--- src/widget/dropdown/Select.ts
+++ src/widget/dropdown/Select.ts
@@ -1,13 +1,4 @@
 import * as m from "mithril";
-/*
-import {
-	DropdownFrame,
-	DropdownMenuFrame,
-	DropdownMenuFrameAttr,
-	SelectText,
-	SelectTextAttr
-	} from "../../element/dropdown";
-*/
 import { Just, Maybe, Nothing } from "purify-ts";
 import { AnimateFrame } from "../../abstract/animate";
 import { Outter, OutterAttr } from "../../abstract/outter";
@@ -46,44 +37,6 @@ export interface SelectAttr<T> {
 }
 
 export const Select = <T>(): m.Component<SelectAttr<T>> => {
-	/*
-	const stateRef = new IORef<boolean>(false);
-
-	return {
-		view: ({ attrs }) => {
-			const textAttr: SelectTextAttr<T> = {
-				text: Maybe.fromNullable(attrs.value).join().extract(),
-				placeholder: attrs.placeholder,
-				renderText: attrs.renderText
-			};
-
-			const mchange = Maybe.fromNullable(attrs.connectChange);
-
-			const connectRemove = () => {
-				mchange.ifJust(f => f(Nothing));
-			};
-
-			const menuAttr: DropdownMenuFrameAttr<T> = {
-				value: stateRef,
-				itemList: attrs.itemList ?? [],
-				renderItem: attrs.renderItem ?? String,
-				el: "div.ui.menu.selection.transition.visible",
-				connectClick: item => {
-					stateRef.put(false);
-					Just(Just(item)).ap(mchange)
-				}
-			};
-
-			return m(DropdownFrame, { value: stateRef, klass: Nothing }, [
-				m("i.icon.dropdown"),
-				m("i.icon.remove", { onclick: connectRemove }),
-				m<SelectTextAttr<T>, {}>(SelectText, textAttr),
-				m<DropdownMenuFrameAttr<T>, {}>(DropdownMenuFrame, menuAttr)
-			]);
-		}
-		};
-	*/
-
 	const state = mutable<boolean>(false);
 
 	const outterAttr: OutterAttr = {
@@ -105,6 +58,16 @@ export const Select = <T>(): m.Component<SelectAttr<T>> => {
 				]),
 				onclick: toggleE
 			};
+
+			const textBox = Maybe.fromNullable(attrs.value)
+				.join()
+				.caseOf({
+					Just: t => {
+						const render = attrs.renderText ?? String;
+						return m("div.text", render(t));
+					},
+					Nothing: () => m("div.default.text", attrs.placeholder)
+				});
 
 			const removeIcon = Maybe.fromFalsy(attrs.isShowRemoveIcon ?? true)
 				.map(_ => {
@@ -129,7 +92,7 @@ export const Select = <T>(): m.Component<SelectAttr<T>> => {
 
 			return m(Outter, outterAttr, [
 				m("div.ui.selection.dropdown", dropdownAttr, [
-					m("div.text.default", "请选择一个选项"),
+					textBox,
 					m("i.icon.dropdown"),
 					removeIcon.extract(),
 					dropdownMenu.extract()
