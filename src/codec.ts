41ea5e843f104202ca15399c82febd2939ca9213
荀洪道 <kalxd@126.com>
2024-05-15 19:29:49 +0800

    添加两个类型计算。

--- src/codec.ts
+++ src/codec.ts
@@ -1,1 +1,34 @@
 export * from "purify-ts/Codec";
+
+// _a_ab => _aAb
+// a_ab => aAb
+type ToCamelCase<T> = T extends `_${infer Rest}`
+	? `_${ToCamelCase<Rest>}`
+	: T extends `${infer First}_${infer Rest}`
+	? `${First}${Capitalize<ToCamelCase<Rest>>}` : T;
+
+// _aAb => _a_ab
+// aAb => a_ab
+type ToSnakeCase<T> = T extends `_${infer Rest}`
+	? `_${ToSnakeCase<Rest>}`
+	: T extends `${infer FirstChar}${infer Rest}`
+	? `${FirstChar extends Capitalize<FirstChar> ? "_" : ""}${Lowercase<FirstChar>}${ToSnakeCase<Rest>}`
+	: T;
+
+/*
+const takeUnderscoreWhile = <T extends string>(key: T): [T, T] => {
+	const prefix = "";
+};
+
+const toCamelCaseString = <T extends string>(key: T): ToCamelCase<T> => {
+};
+
+const toCamelCase = <T, K extends keyof T & string>(input: T): ToCamelCase<T> => {
+	let result = {} as ToCamelCase<T>;
+
+	for (const k in input) {
+	}
+
+	return result;
+};
+*/
