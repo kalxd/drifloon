613228d02ed1876962a9637e17e716b95c29ea3d
荀洪道 <kalxd@126.com>
2024-05-15 19:39:53 +0800

    列表操作。

--- src/data/list.ts
+++ src/data/list.ts
@@ -1,7 +1,9 @@
 import { Nothing, Maybe } from "purify-ts";
-import { List as StdList } from "purify-ts";
+import { List as StdList } from "purify-ts/List";
+import { NonEmptyList as StdNonEmptyList } from "purify-ts/NonEmptyList";
 
-const findMap = <T, R>(f: (item: T) => Maybe<R>, xs: Array<T>): Maybe<R> => {
+/** findMap */
+const list_findMap = <T, R>(f: (item: T) => Maybe<R>, xs: Array<T>): Maybe<R> => {
 	for (const x of xs) {
 		const a = f(x);
 
@@ -13,7 +15,37 @@ const findMap = <T, R>(f: (item: T) => Maybe<R>, xs: Array<T>): Maybe<R> => {
 	return Nothing;
 };
 
+const nonlist_findMap = <T, R>(f: (item: T) => Maybe<R>, xs: StdNonEmptyList<T>): Maybe<R> =>
+	list_findMap(f, xs);
+/** end findMap */
+
+/** takeWhile */
+const list_takeWhile = <T>(f: (item: T) => boolean, xs: Array<T>): Array<T> => {
+	let result = [];
+	for (const x of xs) {
+		if (f(x)) {
+			result.push(x);
+		}
+		else {
+			return result;
+		}
+	}
+
+	return result;
+};
+
+const nonlist_takeWhile = <T>(f: (item: T) => boolean, xs: StdNonEmptyList<T>): Array<T> =>
+	list_takeWhile(f, xs);
+/** end takeWhile */
+
 export const List = {
 	...StdList,
-	findMap
+	findMap: list_findMap,
+	takeWhile: list_takeWhile
+};
+
+export const NonEmptyList = {
+	...StdNonEmptyList,
+	findMap: nonlist_findMap,
+	takeWhile: nonlist_takeWhile
 };
