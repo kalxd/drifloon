ce30e00489dfd48e202dba6d6e405b95df198c78
荀洪道 <kalxd@126.com>
2025-08-07 15:14:33 +0800

    .

---
+++ projects/drifloon/src/lib/element/button/button.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'ui-button',
+	imports: [],
+	templateUrl: './button.html',
+	styleUrl: './button.css'
+})
+export class Button {
+
+}
