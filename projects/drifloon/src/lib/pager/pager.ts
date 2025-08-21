46bd8019d2bd91a5ff71d4e3cbdc344a71920e33
荀洪道 <kalxd@126.com>
2025-08-21 16:08:21 +0800

    简单的长度计算。

--- projects/drifloon/src/lib/pager/pager.ts
+++ projects/drifloon/src/lib/pager/pager.ts
@@ -1,9 +1,16 @@
-import { Component, input, output } from '@angular/core';
+import { Component, computed, input, output, Signal } from '@angular/core';
 
 export interface PagerInput {
 	page: number;
 	size: number;
 	count: number;
+}
+
+interface PagerResult {
+	page: number;
+	prevPage: number;
+	nextPage: number;
+	totalPage: number;
 }
 
 @Component({
@@ -16,4 +23,22 @@ export class UiPager {
 	pager = input.required<PagerInput>();
 
 	pageChange = output<number>();
+
+	protected pagerResult: Signal<PagerResult> = computed(() => {
+		const pager = this.pager();
+		const totalPage = Math.ceil(pager.count / pager.size);
+		const prevPage = Math.max(1, pager.page - 1);
+		const nextPage = Math.min(totalPage, pager.page + 1);
+
+		return {
+			page: pager.page,
+			prevPage,
+			nextPage,
+			totalPage,
+		};
+	});
+
+	protected connectToPage(page: number): void {
+		this.pageChange.emit(page);
+	}
 }
--- projects/site/src/app/page/pager/pager.ts
+++ projects/site/src/app/page/pager/pager.ts
@@ -17,11 +17,22 @@ export class SitePager {
 	size = signal(10);
 	count = signal(100);
 
+	protected changeHistory = signal<Array<number>>([]);
+
 	protected thePager(): PagerInput {
 		return {
 			page: this.page(),
 			count: this.count(),
 			size: this.size()
 		};
+	}
+
+	protected connectPageChange(page: number): void {
+		this.changeHistory.update(xs => {
+			xs.unshift(page);
+			return xs;
+		});
+
+		this.page.set(page);
 	}
 }
