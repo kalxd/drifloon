6462651a77206b2bf0429ca76055c4572b91f2a2
荀洪道 <kalxd@126.com>
2025-08-22 10:12:56 +0800

    一个简易的对话框。

--- projects/site/src/app/page/dialog/dialog.ts
+++ projects/site/src/app/page/dialog/dialog.ts
@@ -1,19 +1,29 @@
 import { Component, viewChild } from '@angular/core';
-import { UiDialog, UiTopbar } from 'drifloon';
+import { UiDialog, UiDivider, UiTopbar } from 'drifloon';
+import { PageForm, PageFormResult } from './form';
 
 @Component({
 	selector: 'site-dialog',
 	imports: [
 		UiDialog,
-		UiTopbar
+		UiTopbar,
+		UiDivider,
+
+		PageForm
 	],
 	templateUrl: './dialog.html',
 	styleUrl: './dialog.css'
 })
 export class SiteDialog {
 	protected plainDialog = viewChild.required<UiDialog<void>>("plain");
+	protected formDialog = viewChild.required<UiDialog<void>>("form");
 
 	protected connectPlainOk(): void {
 		alert("你点击了确定！");
+	}
+
+	protected connectFormOk(value: PageFormResult): void {
+		this.formDialog().close();
+		alert(JSON.stringify(value, null, 4));
 	}
 }
