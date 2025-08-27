738dec8402b690676a805371accecae9e6db766e
荀洪道 <kalxd@126.com>
2025-08-27 14:43:56 +0800

    添加更适合的skeleton。

--- projects/drifloon/src/lib/skeleton/skeleton.ts
+++ projects/drifloon/src/lib/skeleton/skeleton.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+
+type Suit = "page" | "dialog";
 
 @Component({
 	selector: 'ui-skeleton',
@@ -7,4 +9,12 @@ import { Component } from '@angular/core';
 	styleUrl: './skeleton.css'
 })
 export class UiSkeleton {
+	suit = input<Suit>("page");
+
+	theClass(): Record<string, boolean> {
+		return {
+			page: this.suit() === "page",
+			dialog: this.suit() === "dialog"
+		};
+	}
 }
