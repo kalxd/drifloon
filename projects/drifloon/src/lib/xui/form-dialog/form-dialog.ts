8b83fcac0d0d4f3530eddf2a303f9ec5f624bda8
阮鹤民 <kalxd@126.com>
2026-08-29 16:02:49 +0800

    基本设想。

--- projects/drifloon/src/lib/xui/form-dialog/form-dialog.ts
+++ projects/drifloon/src/lib/xui/form-dialog/form-dialog.ts
@@ -1,27 +1,38 @@
 import { Component, computed, input, output, signal, viewChild } from '@angular/core';
 import { UiDialog } from '../../dialog/dialog';
-import { XUiForm, XUiFormState } from "../form/form";
-import { UiSkeleton } from '../../ui/skeleton/skeleton';
 import { UiDialogBox } from '../../box/dialog-box';
+import { UiBaseForm, UiBaseFormAction } from "../../ui/base-form/base-form";
 import * as R from "rxjs";
+import * as Ar from "../../data/async-result";
 import { FieldTree } from '@angular/forms/signals';
+
+interface XUiFormDialogState {
+	title?: string;
+	isLoading?: boolean;
+	okText?: string;
+	cancelText?: string;
+}
 
 @Component({
 	imports: [
 		UiDialog,
-		XUiForm,
-		UiSkeleton,
-		UiDialogBox
+		UiDialogBox,
+		UiBaseForm,
+		UiBaseFormAction
 	],
 	selector: 'xui-form-dialog',
 	styleUrl: './form-dialog.css',
 	templateUrl: './form-dialog.html',
 })
 export class XUiFormDialog {
-	readonly state = input<XUiFormState>({});
+	readonly state = input<XUiFormDialogState>({});
 	readonly submit = output<void>();
 
 	protected readonly isLoading = computed(() => this.state().isLoading === true);
+	protected readonly title = computed(() => this.state().title ?? "请填写");
+	protected readonly okText = computed(() => this.state().okText ?? "好");
+	protected readonly cancelText = computed(() => this.state().cancelText ?? "不好");
+
 	private readonly dialogRef = viewChild.required(UiDialog);
 
 	show(): void {
@@ -33,11 +44,50 @@ export class XUiFormDialog {
 	}
 }
 
+@Component({
+	template: ""
+})
 export abstract class XUiBaseFormDialog<T, R> {
 	protected title: string | undefined;
 	private readonly isLoading = signal<boolean>(false);
 
 	protected readonly abstract fd: FieldTree<T>;
+	private readonly ok$ = new R.Subject<R>();
+	private readonly dialogRef = viewChild.required(XUiFormDialog);
 
 	protected init(_: T): void {}
+
+	abstract sumbit(): R.Observable<Ar.AsyncResult<R, unknown>>;
+
+	connectSubmit(): void {
+		if (this.fd().invalid()) {
+			return ;
+		}
+
+		this.sumbit()
+			.pipe(
+				R.startWith(Ar.mkAsyncRefresh),
+				R.tap(x => Ar.caseOfAsyncResult(x, {
+					refresh: () => this.isLoading.set(true),
+					finish: x => {
+						this.isLoading.set(false);
+						this.ok$.next(x);
+						this.dialogRef().close();
+					},
+					err: _ => this.isLoading.set(false)
+				}))
+			)
+			.subscribe(Ar.subscriptionAll);
+	}
+
+	show(input: T): R.Observable<R> {
+		this.init(input);
+
+		this.dialogRef().show();
+
+		return R.of().pipe(
+			R.switchMap(_ => this.ok$),
+			R.take(1)
+		);
+	}
 }
